7066c6329169fa9f5cf82410df771922c95a654e
brandondeveloper <brandonochieng72@gmail.com>
2025-11-09 14:55:13 +0300

    Reorganize project structure: move all backend files to backend/ folder

--- frontend/src/components/ui/Card.tsx
+++
@@ -1,8 +0,0 @@
-// Card Components
-export * from './card/base-card'
-export * from './card/stat-card'
-export * from './card/badge'
-export * from './card/progress-bar'
-
-// Re-export for convenience
-export { Badge, StatusBadge, RiskBadge } from './card/badge'
